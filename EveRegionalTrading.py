eed562ae9b8384719dd5b34d8f2d12cbdc675a2f
Austin Briggs <awbdallas@gmail.com>
2016-02-14 12:12:51 -0600

    Updates

--- EveRegionalTrading.py
+++ EveRegionalTrading.py
@@ -1,58 +1,95 @@
 import xml.etree.ElementTree as ET
 import urllib
+import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
+from contextlib import closing
 
 #config
 DEBUG      = True
 SECRET_KEY = 'development key'
+DATABASE = '/tmp/flaskr.db'
 
 #For the actual program
 EVE_ITEMS_CSV = '/home/awbriggs/Development/EveRegionalTrading/eve_items/eve_items.csv'
-EVE_ITEMS_NAME_DICT = {}
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def get_input():
-    return render_template('index.html')
+    cur = g.db.execute('select title, text from reports order by id desc')
+    reports = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('index.html', reports=reports)
 
 @app.route('/report',methods=['POST'])
 def add_query():
-    EVE_ITEMS_TYPEID_DICT, EVE_ITEMS_NAME_DICT = get_items()
+    eve_items_typeid_dict, eve_items_name_dict = get_items()
 
     report_title  = request.form['report_name']
     input_items   = request.form['items_text']
     first_system  = request.form['first_system_id']
     second_system = request.form['second_system_id']
 
-    input_items = parse_input(input_items, EVE_ITEMS_NAME_DICT)
+    g.db.execute('insert into reports (title, text) values (?, ?)',
+                 [report_title, input_items.replace('\n', '<br />')])
+    g.db.commit()
+
+    input_items = parse_input(input_items, eve_items_name_dict)
 
     first_system_prices,second_system_prices = \
         get_market_information(input_items,first_system,second_system)
 
 
     return render_template('report.html',input_items=input_items, first_system_prices=first_system_prices,
-                           second_system_prices=second_system_prices, EVE_ITEMS_TYPEID_DICT=EVE_ITEMS_TYPEID_DICT)
+                           second_system_prices=second_system_prices, eve_items_typeid_dict=eve_items_typeid_dict)
 
 def get_items():
 
-    holding_name_dict ={}
-
+    holding_name_dict = {}
+    holding_typeid_dict = {}
 
     for line in open(EVE_ITEMS_CSV):
         line = line.strip('\r\n')
         #TYPEID,GROUPID,TYPENAME,VOLUME
         holding_array = line.split(',')
         holding_name_dict[holding_array[2]] = {
-            'TYPEID'   : holding_array[0],
-            'GROUPID'     : holding_array[1],
+            'TYPEID'  : holding_array[0],
+            'GROUPID' : holding_array[1],
+            'VOLUME'  : holding_array[3]
+        }
+
+    for line in open(EVE_ITEMS_CSV):
+        line = line.strip('\r\n')
+        #TYPEID,GROUPID,TYPENAME,VOLUME
+        holding_array = line.split(',')
+        holding_typeid_dict[holding_array[0]] = {
+            'TYPENAME' : holding_array[2],
+            'GROUPID'  : holding_array[1],
             'VOLUME'   : holding_array[3]
         }
 
-    return holding_name_dict
+    return holding_typeid_dict, holding_name_dict
 
 def get_market_information(input_items,first_system,second_system):
     first_url,second_url = make_url(input_items, first_system, second_system)
@@ -101,7 +138,7 @@ def make_url(input_items,first_system,second_system):
 
     return first_url, second_url
 
-def parse_input(raw_items, EVE_ITEMS_NAME_DICT):
+def parse_input(raw_items, eve_items_name_dict):
     holding_items = []
     final_list = []
     raw_items = raw_items.encode('ascii', 'ignore')
@@ -113,8 +150,8 @@ def parse_input(raw_items, EVE_ITEMS_NAME_DICT):
 
     for item in holding_items:
         try:
-            if EVE_ITEMS_NAME_DICT[item]:
-                final_list.append(EVE_ITEMS_NAME_DICT[item]['TYPEID'])
+            if eve_items_name_dict[item]:
+                final_list.append(eve_items_name_dict[item]['TYPEID'])
         except KeyError:
             continue
 
