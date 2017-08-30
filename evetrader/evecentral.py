240d7e9ca623470705ebbfe7f98b17531a0a4706
Austin <awbdallas@gmail.com>
2017-08-29 19:17:36 -0500

    Did I actually forget all of the files

---
+++ evetrader/evecentral.py
@@ -0,0 +1,39 @@
+from requests import get
+from config import EVE_CENTRAL_MARKETSTAT_ENDPOINT
+
+
+class EveCentral:
+
+
+    def __init__(self):
+        pass
+
+
+    def get_price_information(self, list_of_items, systems):
+        print(list_of_items)
+        urls = []
+        for system in systems:
+            urls.append(self.build_url(list_of_items, system))
+
+        print(urls)
+        results = []
+        for url in urls:
+            results.append(self.make_request(url))
+
+        print(results)
+        return results
+
+
+    def build_url(self, list_of_items, system):
+        base_url = EVE_CENTRAL_MARKETSTAT_ENDPOINT +\
+                '&'.join(['typeid=' + typeid for typeid in list_of_items])
+        base_url += '&usesystem=' + system
+
+        return base_url
+
+
+    def make_request(self, url):
+        result = get(url)
+
+        if result.status_code == 200:
+            return result.json()
